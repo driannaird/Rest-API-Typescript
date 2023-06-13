5089d69ed61a6bb6c3e4fc1b6b5ec23af922f350
Drian-key <riskidrian1@gmail.com>
2023-06-13 19:47:17 +0700

    Initialize Project

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+import express, {Application, NextFunction, Request, Response} from "express";
+
+const app: Application = express();
+const port: Number = 4000;
+
+app.use("/health", (req: Request, res: Response, next: NextFunction) => {
+    res.status(400).send({status: "200"})
+});
+
+app.listen(port, () => console.log(`Server is listen on port ${port}`))
