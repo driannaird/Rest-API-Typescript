d30e7f67dcebdee176fe3deb0703be41ef30fdf5
Drian-key <riskidrian1@gmail.com>
2023-06-14 06:37:43 +0700

    fix linter

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
-import express, { Application, NextFunction, Request, Response } from "express"
+import express, { Application, NextFunction, Request, Response } from 'express'
 
 const app: Application = express()
-const port: Number = 4000
+const port: number = 4000
 
-app.use("/health", (req: Request, res: Response, next: NextFunction) => {
-  res.status(400).send({ status: "200", data: "hello world" })
+app.use('/health', (req: Request, res: Response, next: NextFunction) => {
+  res.status(400).send({ status: '200', data: 'hello world' })
 })
 
 app.listen(port, () => console.log(`Server is listen on port ${port}`))
