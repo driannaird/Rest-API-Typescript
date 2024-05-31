1d719f59a9dd184c8a2d2b404808b6d2aed1b75d
Chopper <riskidrian1@gmail.com>
2024-05-31 22:58:11 +0700

    chore: non active middleware

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,7 +3,7 @@ import { HealthRouter } from './health.route'
 import { ProductRouter } from './product.route'
 import { SiswaRouter } from './siswa.route'
 import { UserRouter } from './user.route'
-import { TaskRouter } from './task.route'
+// import { TaskRouter } from './task.route'
 
 const _routes: Array<[string, Router]> = [
   ['/health', HealthRouter],
